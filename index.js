01410b71d60b6edd1daf2f8cd12778493c934867
VladimirGlushchenko <105595545+VladimirGlushchenko@users.noreply.github.com>
2022-12-03 20:26:51 +0300

    add lodash

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello, Hexlet!");
+import _ from "lodash";
+
+// Пересечение массивов
+_.intersection([2, 1], [2, 3]); // => [2]
+
+// capitalize делает первую букву заглавной
+_.capitalize("hello"); // Hello
+console.log(_.last(["one", "two"]));
